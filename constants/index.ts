802bf64cde28680a1e7fdd0e27c598560f3a0168
Tha_UI_Guy <adeniran1059@gmail.com>
2023-08-04 20:50:43 +0200

    WIP

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Horpey Dev'
+export const appDescription = 'Horpey Dev - A Full Stack Developer'
